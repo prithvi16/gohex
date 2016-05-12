2c2a20223d60baad0761ec6403db977b94815cf5
pvsukale <pvsukale@gmail.com>
2016-05-13 00:45:30 +0530

    added error handling

--- main.go
+++ main.go
@@ -4,11 +4,25 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 )
 
 func main() {
-	filename := os.Args[1]
-	data, _ := ioutil.ReadFile(filename)
-	fmt.Println(hex.Dump(data))
+	if len(os.Args) == 1 {
+		fmt.Println("usage: gohex  <filename>")
+
+		os.Exit(1)
+
+	} else {
+		filename := os.Args[1]
+
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(hex.Dump(data))
+
+	}
+
 }
